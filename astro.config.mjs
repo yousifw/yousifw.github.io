4a54f20fab0f8ca886ce054b737a5ae31ecc6a0f
ywrd26 <yousif.wardah@mni.thm.de>
2025-10-07 22:44:49 +0200

    Add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://yousifw.github.io",
-    experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "Roboto",
-            cssVariable: "--font-roboto",
-        }]
-    }
+  site: "https://yousifw.github.io",
+
+  experimental: {
+      fonts: [{
+          provider: fontProviders.google(),
+          name: "Roboto",
+          cssVariable: "--font-roboto",
+      }]
+  },
+
+  integrations: [mdx()]
 });
